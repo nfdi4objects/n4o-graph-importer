d881ff653503de7008808a216d9621a7bcbe6543
Josef Heers <josef.heers@gbv.de>
2025-07-14 13:11:26 +0200

    Refactor app.py to enhance subprocess handling and improve JSON responses

--- app.py
+++ app.py
@@ -1,22 +1,62 @@
-from flask import Flask, render_template, redirect, request, make_response, url_for
+from flask import Flask, jsonify
 from waitress import serve
 import argparse as AP
-import requests
 import subprocess
+import csv
+
+collections_file = 'stage/collection/collections.csv'
+collection_list = []
+
+
+def load_collections():
+    with open(collections_file) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+        return [row for row in reader]
+    return []
+
 
 app = Flask(__name__, template_folder='templates',
             static_folder='static', static_url_path='/assets')
-app.secret_key = 'your_secret_key'  # Replace with a strong secret key
 
+def run_subprocess(args,timeout=20):
+    """Run a subprocess command and return the output."""
+    try:
+        res = subprocess.run(args, capture_output=True, text=True, check=True,timeout=timeout)
+        return res.stdout, res.stderr
+    except FileNotFoundError as exc:
+        return None,f"Process failed because the executable could not be found.\n{exc}"
+    except subprocess.CalledProcessError as exc:
+        return exc.stdout, exc.stderr
+    except subprocess.TimeoutExpired as exc:
+        return None, f"Process timed out.\n{exc}"
+    
 
 @app.route('/')
 def home():
-    correct = subprocess.run(['echo', 'Josef Heers'],
-                             capture_output=True, text=True)
-    return f'Hey ho {correct.stdout}\n'
+    res,err =run_subprocess(['uname', '-a'])
+    return jsonify(result=res, err=err)
+
+@app.route('/collections')
+def collections():
+    return jsonify(collection_list)
+
+
+def find_p(lst, p=lambda x: True):
+    '''Find object in list by predicate'''
+    return next((x for x in lst if p(x)), None)
+
+
+@app.route('/download/<int:id>')
+def download(id):
+    hasId =  lambda x: x['id'] == str(id)
+    if coll := find_p(collection_list,hasId):
+        url = coll['url']
+        return jsonify(url=url)
+    return jsonify(error="Collection not found",id=id), 404
 
 
 if __name__ == '__main__':
+    collection_list = load_collections()
     parser = AP.ArgumentParser()
     parser.add_argument(
         '-w', '--wsgi', action=AP.BooleanOptionalAction, help="Use WSGI server")
@@ -24,7 +64,6 @@ if __name__ == '__main__':
                         default=5020, help="Server port")
     args = parser.parse_args()
     opts = {"port": args.port}
-
     if args.wsgi:
         serve(app, host="0.0.0.0", **opts)
     else:
