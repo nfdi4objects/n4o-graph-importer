cb85dc27e6f2a45ab8d838bb818a42a7444ddb51
Josef Heers <josef.heers@gbv.de>
2025-07-16 08:33:44 +0200

    REST Api

--- app.py
+++ app.py
@@ -15,30 +15,17 @@ def load_collections():
     return []
 
 
-app = Flask(__name__, template_folder='templates',
-            static_folder='static', static_url_path='/assets')
-
-def run_subprocess(args,timeout=20):
+def run_subprocess(args, timeout=20):
     """Run a subprocess command and return the output."""
     try:
-        res = subprocess.run(args, capture_output=True, text=True, check=True,timeout=timeout)
+        res = subprocess.run(args, capture_output=True, text=True, check=True, timeout=timeout)
         return res.stdout, res.stderr
     except FileNotFoundError as exc:
-        return None,f"Process failed because the executable could not be found.\n{exc}"
+        return None, f"Process failed because the executable could not be found.\n{exc}"
     except subprocess.CalledProcessError as exc:
         return exc.stdout, exc.stderr
     except subprocess.TimeoutExpired as exc:
         return None, f"Process timed out.\n{exc}"
-    
-
-@app.route('/')
-def home():
-    res,err =run_subprocess(['uname', '-a'])
-    return jsonify(result=res, err=err)
-
-@app.route('/collections')
-def collections():
-    return jsonify(collection_list)
 
 
 def find_p(lst, p=lambda x: True):
@@ -46,13 +33,38 @@ def find_p(lst, p=lambda x: True):
     return next((x for x in lst if p(x)), None)
 
 
-@app.route('/download/<int:id>')
-def download(id):
-    hasId =  lambda x: x['id'] == str(id)
-    if coll := find_p(collection_list,hasId):
+app = Flask(__name__, template_folder='templates',
+            static_folder='static', static_url_path='/assets')
+
+
+@app.route('/')
+def home():
+    '''Home page'''
+    return jsonify(message="Welcome to the Collection API!")
+
+
+@app.route('/ping')
+def ping():
+    '''Ping endpoint to check server status'''
+    res, err = run_subprocess(['uname', '-a'])
+    return jsonify(result=res, err=err)
+
+
+@app.route('/collections')
+def collections():
+    '''List all collections'''
+    return jsonify(collection_list)
+
+
+@app.route('/import_collection/<int:id>')
+def import_collection(id):
+    '''Import a collection by ID'''
+    def hasId(x):
+        return x['id'] == str(id)
+    if coll := find_p(collection_list, hasId):
         url = coll['url']
         return jsonify(url=url)
-    return jsonify(error="Collection not found",id=id), 404
+    return jsonify(error="Collection not found", id=id), 404
 
 
 if __name__ == '__main__':
