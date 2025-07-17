c2a065918507bd90d281caf04f3d11f6e5c7ee40
Jakob Voss <voss@gbv.de>
2025-07-17 13:25:03 +0200

    Update receive-collection script with access & format

--- js/csv2json.js
+++ js/csv2json.js
@@ -9,9 +9,16 @@ process.stdin
       if (data[key] === "") delete data[key]
     }
     if ("id" in data) {                 // collections = named graphs
-      data.uri = "https://graph.nfdi4objects.net/collection/" + data.id
+      data.uri = `https://graph.nfdi4objects.net/collection/${data.id}`
       data.type = ["fabio:Database","dcat:Dataset"]
       data.partOf = ["https://graph.nfdi4objects.net/collection/"]
+      if (data.access) {
+        data.access = {
+          url: data.access,
+          format: `https://format.gbv.de/${data.format}`
+        }
+      }
+      delete data.format
     } else if ("wikidata" in data) {    // databases
       data.id = "http://www.wikidata.org/entity/" + data.id
       // TODO: type
